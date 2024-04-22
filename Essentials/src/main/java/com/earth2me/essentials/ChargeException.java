3e687c049b93e9ce563f1b1ec346342bd91f096c
bctweet <94938415+bctweet@users.noreply.github.com>
2024-04-21 20:17:02 -0500

    Issue stems from previous exception handling not being translatable, adapting charge exceptions to be translatable.

--- Essentials/src/main/java/com/earth2me/essentials/ChargeException.java
+++ Essentials/src/main/java/com/earth2me/essentials/ChargeException.java
@@ -1,11 +1,11 @@
 package com.earth2me.essentials;
-
-public class ChargeException extends Exception {
+import net.ess3.api.TranslatableException; // Issue stems from exceptions not being universally translatable
+public class ChargeException extends TranslatableException {
     public ChargeException(final String message) {
         super(message);
     }
 
-    public ChargeException(final String message, final Throwable throwable) {
+    public ChargeException(final String message, final Object... throwable) {
         super(message, throwable);
     }
 }
